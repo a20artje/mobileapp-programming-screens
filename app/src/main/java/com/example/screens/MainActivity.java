9793afc16a04c013de02419449e7dadfc146c3fa
a20artje <coolvids.bruh@gmail.com>
2024-04-15 14:54:23 +0200

    added start new activity logic to button

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
+    int amountOfCookies = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.Button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                amountOfCookies++;
+                Intent intent = new Intent(MainActivity.this, CookieActivity.class);
+                intent.putExtra("name", "Amount of Cookies: ");
+                intent.putExtra("number", amountOfCookies);
+                startActivity(intent);
+            }
+        });
     }
+
+
+
+
+
 }
